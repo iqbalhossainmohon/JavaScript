2d1a2b61ea7832c6dfc14056550428651febed51
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2025-09-08 12:36:46 +0600

    task 2 successfully completed

---
+++ task02.js
@@ -0,0 +1,11 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMarks = mathematics + biology + chemistry + physics + bangla;
+var averageMarks = totalMarks / 5;
+var averageMarksFixed = averageMarks.toFixed(2);
+
+console.log(averageMarksFixed);
