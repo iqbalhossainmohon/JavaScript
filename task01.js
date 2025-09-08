e6525260d118bb8110c1f60b15029e8a6b5d0427
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2025-09-08 12:31:04 +0600

    task 1 successfully completed

---
+++ task01.js
@@ -0,0 +1,8 @@
+var youHave = 1000;
+var orangePrice = 350;
+var applePrice = 220;
+
+var totalPrice = orangePrice + applePrice;
+var canBuy = youHave - totalPrice;
+
+console.log("I have " + canBuy + " taka yen.");
