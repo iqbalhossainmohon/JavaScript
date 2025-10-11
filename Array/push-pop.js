eab5761338cef4c17b4c993545d50faf999e0b08
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2025-10-11 12:36:54 +0600

    Array push pop added

---
+++ Array/push-pop.js
@@ -0,0 +1,22 @@
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// The push() method adds elements to the end of an array
+friends.push('Jay');
+console.log(friends);
+
+friends.push('John', 'Mary');
+console.log(friends);
+
+// The pop() method removes the last element from an array
+friends.pop();
+console.log(friends);
+
+
+// The shift() method removes the first element from an array 
+friends.shift();
+console.log(friends);
+
+// The unshift() method adds elements to the beginning of an array
+friends.unshift('Michael');
+console.log(friends);
