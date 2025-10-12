6e354fb6c5831143ba7f33f32c5f1c45a9cabbcd
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2025-10-12 12:30:43 +0600

    array task 2 complate

---
+++ Array/task2.js
@@ -0,0 +1,7 @@
+const touristSpots = ['Cox\'s Bazar', 'Sajek', 'Bandarban'];
+touristSpots.push('Saint Martin');
+touristSpots.push('Kuakata', 'Sundarbans'); 
+
+touristSpots.pop();
+console.log(touristSpots);
+
