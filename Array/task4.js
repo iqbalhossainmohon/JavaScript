c874e3650e05202bd4d491ae5e43ca99a88f22d8
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2025-10-13 14:50:20 +0600

    array task 4 complete

---
+++ Array/task4.js
@@ -0,0 +1,6 @@
+const name = 'javascript';
+const price = 250;
+const books = ['python', 'javascript', 'html', 'css', 'react'];
+
+console.log(Array.isArray(name));
+console.log(Array.isArray(books));
