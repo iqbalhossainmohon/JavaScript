0fc220e57e94b08c5bb739f0783c73578d7f1ef9
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2025-10-13 14:42:11 +0600

    task3 complete

---
+++ Array/task3.js
@@ -0,0 +1,8 @@
+const books = ['python', 'javascript', 'html', 'css', 'react'];
+
+if(books.includes('javascript')) {
+      console.log('I love javascript');
+}
+else {
+      console.log('I love programming');
+}
