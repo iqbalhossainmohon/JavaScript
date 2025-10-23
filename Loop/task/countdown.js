5357e08700916e2c2405df4628c8f609243a0bb7
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2025-10-23 12:20:14 +0600

    Implement a countdown timer that counts down from 21 to 15.

---
+++ Loop/task/countdown.js
@@ -0,0 +1,9 @@
+/***
+Implement a countdown timer that counts down from 21 to 15.
+*/
+
+let countdownStart = 21;
+while (countdownStart >= 15) {
+    console.log(countdownStart);
+    countdownStart--;
+}
